1ca840d4a6e712c12664458d94d6fbb0b9322747
cwwilson08 <33009556+cwwilson08@users.noreply.github.com>
2018-05-23 14:05:56 -0700

    powerstrip support

--- DeviceHandlers/TuyAPI_Smart.groovy
+++ DeviceHandlers/TuyAPI_Smart.groovy
@@ -16,31 +16,21 @@ Unless required by applicable law or agreed to in writing, software distributed 
 Supported models and functions:  This device supports smart plugs that use the Tuya Smart Life app
 
 Update History
+05-23-2018 - Updated to control all powerstrip plugs from one device. Fixed status response - should now be accurate. Node script update with changes by Dave Gutheinz.
 05-22-2018 - Updated to make on off commands work and make compatible with Hubitat
 01-04-2018	- Initial release
 */
 metadata {
 	definition (name: "TuyAPI Smart Plug", namespace: "cwwilson08", author: "Chris Wilson") {
-		capability "Switch"
 		capability "Refresh"
-		//capability "Polling"
-		//capability "Sensor"
 		capability "Actuator"
-		//command "setCurrentDate"
-	}
-	tiles(scale: 2) {
-		standardTile("switch", "device.switch", width: 6, height: 4, canChangeIcon: true) {
-        	state "on", label:'${name}', action:"switch.off", icon:"st.switches.switch.on", backgroundColor:"#00a0dc",nextState:"turningOff"
-			state "off", label:'${name}', action:"switch.on", icon:"st.switch.off", backgroundColor:"#ffffff",nextState:"waiting"
-			state "turningOff", label:'waiting', action:"switch.off", icon:"st.switches.switch.on", backgroundColor:"#15EE10",nextState:"waiting"
-			state "waiting", label:'${name}', action:"switch.on", icon:"st.switches.switch.on", backgroundColor:"#15EE10",nextState:"on"
-			state "offline", label:'Comms Error', action:"switch.on", icon:"st.switch.off", backgroundColor:"#e86d13",nextState:"waiting"
-		}
-		standardTile("refresh", "capability.refresh", width: 3, height: 2,  decoration: "flat") {
-			state ("default", label:"Refresh", action:"refresh.refresh", icon:"st.secondary.refresh")
-		}		 
-		main("switch")
-		details("switch", "power", "refresh")
+        command "togglePower", ["NUMBER"]
+        attribute "Plug_1", "string"
+        attribute "Plug_2", "string"
+        attribute "Plug_3", "string"
+        attribute "Plug_4", "string"
+        attribute "Plug_5", "string"
+        attribute "Plug_6", "string"
 	}
 }
 preferences {
@@ -48,7 +38,7 @@ preferences {
 	input(name: "gatewayIP", type: "text", title: "Gateway IP", required: true, displayDuringSetup: true)
 	input(name: "deviceID", type: "text", title: "Device ID", required: true, displayDuringSetup: true)
 	input(name: "localKey", type: "text", title: "Local Key", required: true, displayDuringSetup: true)
-    input(name: "dps", type: "text", title: "dps", required: true, displayDuringSetup: true)
+   // input(name: "dps", type: "text", title: "dps", required: true, displayDuringSetup: true)
 }
 
 def installed() {
@@ -61,49 +51,107 @@ def updated() {
 	runIn(2, refresh)
 }
 //	----- BASIC PLUG COMMANDS ------------------------------------
-def on() {
-	sendCmdtoServer("on", "deviceCommand", "onOffResponse")
+def togglePower(deviceNo) {
+    deviceNo = deviceNo.toString()
+//	deleted the cmds on and off.  Replace with this.
+//	assume that states are on or off.  Offline will try to turn on.
+//	added deviceNo.  This will pass through the system to the response method and allow proper parsing.
+    def plugState = device.currentValue("Plug_${deviceNo}")
+    if (plugState == "on") {
+		sendCmdtoServer("off", deviceNo, "deviceCommand", "onOffResponse")
+    } else {
+		sendCmdtoServer("on", deviceNo, "deviceCommand", "onOffResponse")
+    }
 }
 
-def off() {
-	sendCmdtoServer("off", "deviceCommand", "onOffResponse")
-}
-
-def onOffResponse(response){
-   // log.debug "line 71 ${cmdResponse}"
-   // log.debug "line 71 ${onoff}"
+def onOffResponse(response, deviceNo){
+//    deviceNo = deviceNo.toString()
+//	Changed switch to plug_$(deviceNo}.  device no comes from the original toggle (carried
+//	all commands and responses.
 	if (cmdResponse == "TcpTimeout") {
 		log.error "$device.name $device.label: Communications Error"
-		sendEvent(name: "switch", value: "offline", descriptionText: "ERROR - OffLine - mod onOffResponse")
+        sendEvent(name: "Plug_${deviceNo}", value: "offline", descriptionText: "ERROR - OffLine - mod onOffResponse")
 	} else {
-        def status = onoff
-		log.info "${device.name} ${device.label}: Power: ${status}"
-		sendEvent(name: "switch", value: status)
+
+		log.info "${device.name} ${device.label}: Power: ${response}"
+		sendEvent(name: "Plug_${deviceNo}", value: response)
 	}
 }
-
 //	----- REFRESH ------------------------------------------------
 def refresh(){
-	//sendEvent(name: "switch", value: "waiting", isStateChange: true)
-	sendCmdtoServer("status", "deviceCommand", "refreshResponse")
+//	Will do all six switches on refresh, one second apart.
+    def rfrsh = []
+	rfrsh << sendCmdtoServer("status", "1", "deviceCommand", "refreshResponse")
+	rfrsh << pauseExecution(500)
+	rfrsh << sendCmdtoServer("status", "2", "deviceCommand", "refreshResponse")
+	rfrsh << pauseExecution(500)
+	rfrsh << sendCmdtoServer("status", "3", "deviceCommand", "refreshResponse")
+	rfrsh << pauseExecution(500)
+	rfrsh << sendCmdtoServer("status", "4", "deviceCommand", "refreshResponse")
+    rfrsh << pauseExecution(500)
+    rfrsh << sendCmdtoServer("status", "5", "deviceCommand", "refreshResponse")
+    rfrsh << pauseExecution(500)
+    rfrsh << sendCmdtoServer("status", "6", "deviceCommand", "refreshResponse")
+    rfrsh
 }
-def refreshResponse(response){
+def refreshResponse(cmdResponse, deviceNo){
+    log.debug "cmdresponse ${cmdResponse}"
+    def status
 	if (cmdResponse == "TcpTimeout") {
 		log.error "$device.name $device.label: Communications Error"
-		sendEvent(name: "switch", value: "offline", descriptionText: "ERROR - OffLine - mod onOffResponse")
+		sendEvent(name: "Plug_${deviceNo}", value: "offline", descriptionText: "ERROR - OffLine - mod onOffResponse")
 	} else {
-       // log.debug "line 94 ${cmdResponse}"
         if (cmdResponse == true){
-            def status = on
+            status = "on"
         }else{
-            def status = off
+            status = "off"
+		}
 		log.info "${device.name} ${device.label}: Power: ${status}"
-		sendEvent(name: "switch", value: status)
+		sendEvent(name: "Plug_${deviceNo}", value: status)
 	}
 }
-}
 //	----- SEND COMMAND DATA TO THE SERVER -------------------------------------
-private sendCmdtoServer(command, hubCommand, action){
+
+private sendCmdtoServer(command, deviceNo, hubCommand, action){
+//	added deviceNo to items put in header.
+//	Will add to the node.js script to extract then add to response.
+   // def deviceIP
+   // def deviceID
+    def dps 
+    switch(deviceNo) {
+        case "1":
+    		deviceIP = deviceIP
+    		deviceID = deviceID
+            dps = deviceNo
+        	break
+        case "2":
+    		deviceIP = deviceIP
+    		deviceID = deviceID
+            dps = deviceNo
+        	break
+        case "3":
+    		deviceIP = deviceIP
+    		deviceID = deviceID
+            dps = deviceNo
+        	break
+        case "4":
+    		deviceIP = deviceIP
+    		deviceID = deviceID
+            dps = deviceNo
+        	break
+         case "5":
+    		deviceIP = deviceIP
+    		deviceID = deviceID
+            dps = deviceNo
+        	break
+         case "6":
+    		deviceIP = deviceIP
+    		deviceID = deviceID
+            dps = deviceNo
+        	break
+        default:
+            break
+    }
 	def headers = [:] 
 	headers.put("HOST", "$gatewayIP:8083")	//	SET TO VALUE IN JAVA SCRIPT PKG.
 	headers.put("tuyapi-ip", deviceIP)
@@ -112,6 +160,7 @@ private sendCmdtoServer(command, hubCommand, action){
 	headers.put("tuyapi-command", command)
     headers.put("action", action)
 	headers.put("command", hubCommand)
+    headers.put("deviceNo", deviceNo)
     headers.put("dps", dps)
    // log.debug "${hubCommand}"
 	def hubCmd = new hubitat.device.HubAction([
@@ -119,42 +168,42 @@ private sendCmdtoServer(command, hubCommand, action){
 		headers: headers]
     
 	)
+    log.debug "${hubCmd}"
     hubCmd
     
 }
 
 def parse(response) {
+//	extract also deviceNo from header to pass to parse methods.
 	def resp = parseLanMessage(response)
 	def action = resp.headers["action"]
-   // def onoff = resp.headers["onoff"]
-    //log.debug "line 123 ${onoff}"
+    def deviceNo = resp.headers["deviceNo"]
 	def jsonSlurper = new JsonSlurper()
-	cmdResponse = jsonSlurper.parseText(resp.headers["cmd-response"])
-    onoff = resp.headers["onoff"]
+	def cmdResponse = jsonSlurper.parseText(resp.headers["cmd-response"])
+    def onoff = resp.headers["onoff"]
+log.error onoff
+log.error deviceNo
+log.error cmdResponse
     
-        
-   // log.debug "line 122 ${resp.headers}"
-	//if (cmdResponse == "TcpTimeout") {
     if (cmdResponse == "TcpTimeout") {
 		log.error "$device.name $device.label: Communications Error"
 		sendEvent(name: "switch", value: "offline", descriptionText: "ERROR at hubResponseParse TCP Timeout")
 		sendEvent(name: "deviceError", value: "TCP Timeout in Hub")
 	} else {
-        //log.debug "line 131 ${action} and ${cmdResponse}"
-		actionDirector(action, cmdResponse, onoff)
+        log.debug "line 175 ${action} and ${cmdResponse}"
+		actionDirector(action, cmdResponse, onoff, deviceNo)
 		sendEvent(name: "deviceError", value: "OK")
 	}
 }
-def actionDirector(action, cmdResponse, onoff) {
-    //log.debug "line 139 ${onoff}"
-    
+def actionDirector(action, cmdResponse, onoff, deviceNo) {
 	switch(action) {
 		case "onOffResponse":
-        onOffResponse(onoff)
+        onOffResponse(onoff, deviceNo)
 			break
 
 		case "refreshResponse":
-			refreshResponse(cmdResponse)
+        log.debug "line 188 ${cmdResponse}"
+			refreshResponse(cmdResponse, deviceNo)
 			break
 
 		default:
